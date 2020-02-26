c18353baabdd88798fea2f00621a94aa6ae96832
Koushik Mohan <koushikmohan1996@gmail.com>
2020-02-26 07:36:51 +0530

    Add minitest reporter

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,9 @@
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
+require "minitest/reporters"
+
+Minitest::Reporters.use! [Minitest::Reporters::SpecReporter.new(:color => true)]
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
