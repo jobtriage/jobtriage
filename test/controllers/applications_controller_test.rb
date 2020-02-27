0c7eb1b39d696fb457b5e611f13bc2931e48fbb6
Koushik Mohan <koushikmohan1996@gmail.com>
2020-02-27 14:40:22 +0530

    Improve test assertion

--- test/controllers/applications_controller_test.rb
+++ test/controllers/applications_controller_test.rb
@@ -4,53 +4,53 @@ require 'test_helper'
 
 class ApplicationsControllerTest < ActionDispatch::IntegrationTest
   test 'Should get applications' do
-    user = get_user('test@test.com')
-    token = get_jwt_token(user.id)
-    get applications_url, headers: { 'Authorization' => "bearer #{token}" }
-    assert_response :success
+    get_request applications_url
+    assert_json_response
+  end
+
+  test 'should show job application details' do
+    application = add_application
+    get_request application_url(application)
+
+    resp = assert_json_response
+    assert_json_keys resp, :_id, :company, :description, :location, :status, :priority
   end
 
   test 'Should save job application' do
-    user = get_user('test@test.com')
-    token = get_jwt_token(user.id)
-    post applications_url, headers: { 'Authorization' => "bearer #{token}" }, params: { 
+    post_request applications_url,
       title: 'Software developer',
       status: 'Applied',
       priority: 3,
       company_name: 'Job triage'
-    }
-    assert_response :success
+
+    resp = assert_json_response
+    assert_json_keys resp, :_id, :company, :description, :location, :status, :priority
   end
 
   test 'Should update job details' do
-    user = get_user('test@test.com')
-    token = get_jwt_token(user.id)
-    application = add_application(user)
-    put application_url(application), headers: { 'Authorization' => "bearer #{token}" }, params: { 
+    application = add_application
+    put_request application_url(application),
       title: 'Software dev',
       status: 'applied',
       priority: 1,
       company_name: 'Job triage'
-    }
-    assert_response :success
+
+    resp = assert_json_response
+    assert_json_keys resp, :_id, :company, :description, :location, :status, :priority
   end
 
   test 'Should delete job details' do
-    user = get_user('test@test.com')
-    token = get_jwt_token(user.id)
-    application = add_application(user)
-    delete application_url(application), headers: { 'Authorization' => "bearer #{token}" }
+    application = add_application
+    delete_request application_url(application)
     assert_response :success
   end
 
   test 'Should throw error if all parameters were not passed' do
-    user = get_user('test@test.com')
-    token = get_jwt_token(user.id)
-    post applications_url, headers: { 'Authorization' => "bearer #{token}" }, params: { 
+    post_request applications_url,
       status: 'applied',
       priority: 1,
       company_name: 'Job triage'
-    }
+
     assert_response :internal_server_error
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -10,24 +10,60 @@ class ActiveSupport::TestCase
   parallelize(workers: :number_of_processors)
 
   # Add more helper methods to be used by all tests here...
-  def get_user(email)
-    user = User.where(email: email).first
-    if user
-      return user
+  def get_user
+    if @user
+      return @user
     else
-      user = User.new
-      user.email = 'test@test.com'
-      user.name = 'test'
-      user.password = 'test'
+      @user = User.where(email: 'test@test.com').first
+      if !@user
+        @user = User.new
+        @user.email = 'test@test.com'
+        @user.name = 'test'
+        @user.password = 'test'
+      end
     end
-    return user if user.save
+    return @user if @user.save!
   end
 
   def get_jwt_token(user_id)
     token = JsonWebToken.encode(user_id: user_id.to_s)
   end
 
-  def add_application(user)
-    user.applications.create(title: 'Dev', status: 'applied', priority: 3, company: { name: 'Job triage' })
+  def add_application
+    get_user.applications.create(title: 'Dev', status: 'applied', priority: 3, company: { name: 'Job triage' })
+  end
+
+  def get_request(url)
+    get url, headers: { 'Authorization' => "bearer #{get_jwt_token(get_user.id)}" }
+  end
+
+  def delete_request(url)
+    delete url, headers: { 'Authorization' => "bearer #{get_jwt_token(get_user.id)}" }
+  end
+
+  def post_request(url, data)
+    post url, headers: { 
+      'Authorization' => "bearer #{get_jwt_token(get_user.id)}" 
+    },
+    params: data
+  end
+
+  def put_request(url, data)
+    put url, headers: { 
+      'Authorization' => "bearer #{get_jwt_token(get_user.id)}" 
+    },
+    params: data
+  end
+
+  def assert_json_response
+    assert_response :success
+    json = JSON.parse @response.body, symbolize_names: true
+    assert json, 'Response: ' + @response.body
+    assert json.key?(:message)
+    json[:message]
+  end
+
+  def assert_json_keys(json, *keys)
+    keys.each { |key| assert json.key?(key), json.keys }
   end
 end
