48935060a3632a031eeeae17c833bee2dda7bcf0
Koushik Mohan <koushikmohan1996@gmail.com>
2020-03-01 08:24:07 +0530

    Add notes API

---
+++ test/models/note_test.rb
@@ -0,0 +1,16 @@
+require 'test_helper'
+
+class NoteTest < ActiveSupport::TestCase
+  test 'Notes should not be saved without content' do
+    application = add_application
+    note = application.notes.new
+    assert_not note.save, 'Saved note without content'
+  end
+
+  test 'Notes should be saved with proper details' do
+    application = add_application
+    note = application.notes.new
+    note.content = 'Sample content'
+    assert note.save, 'Failed when creating content'
+  end
+end
