57f84fa5e22d7d876f5f99c5ca12d10ebb0f097d
Koushik Mohan <koushikmohan1996@gmail.com>
2020-03-17 13:39:38 +0000

    Add eslint issue fix (#64)

--- frontend/src/Components/LandingPage/LandingPage.js
+++ frontend/src/Components/LandingPage/LandingPage.js
@@ -4,19 +4,19 @@ import Header from './Header/Header';
 import LandingBody from './LandingBody/LandingBody';
 
 const LandingPage = (props) => {
-    const { email } = props;
-    const isAuthenticated = email !== undefined;
-    return (
-        <div>
-            <Header isAuthenticated={isAuthenticated} />
-            <LandingBody isAuthenticated={isAuthenticated} />
-        </div>
-    );
+  const { email } = props;
+  const isAuthenticated = email !== undefined;
+  return (
+    <div>
+      <Header isAuthenticated={isAuthenticated} />
+      <LandingBody isAuthenticated={isAuthenticated} />
+    </div>
+  );
 };
 
 const mapStateToProps = (state) => {
-    const { users } = state;
-    return users;
+  const { users } = state;
+  return users;
 };
 
 export default connect(mapStateToProps)(LandingPage);
