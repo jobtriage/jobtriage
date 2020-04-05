114823d6ef8febae205e49df3b2c19cf51d7ae23
Koushik Mohan <koushikmohan1996@gmail.com>
2020-04-05 11:39:53 +0530

    Fix Server start up issue due to HOST env variable

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,7 +3,7 @@ class UserMailer < ApplicationMailer
 
   def welcome_email
     @user = params[:user]
-    @url  = url_for controller: 'auth', action: 'verify_email', token: @user.confirm_token, host: ENV['HOST']
+    @url  = url_for controller: 'auth', action: 'verify_email', token: @user.confirm_token, host: ENV['HOST_URL']
     mail(to: @user.email, subject: 'Welcome to Job Triage')
   end
 end
