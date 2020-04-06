46b44195b601073633e95562acb5375184c0c552
Koushik Mohan <koushikmohan1996@gmail.com>
2020-04-06 17:24:38 +0000

    Add change password and forgot password (#88)
    * Add change password and forgot password
    
    * Fix complexity issue

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,4 +6,9 @@ class UserMailer < ApplicationMailer
     @url  = url_for controller: 'auth', action: 'verify_email', token: @user.confirm_token, host: ENV['HOST_URL']
     mail(to: @user.email, subject: 'Welcome to Job Triage')
   end
+
+  def forgot_password_email
+    @user = params[:user]
+    mail(to: @user.email, subject: 'Reset OTP for Job Triage')
+  end
 end
