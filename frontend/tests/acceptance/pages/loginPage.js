6531230e6f9e2f6c5b7c850165105f9617833425
saw-jan <saw.jan.grg3e@gmail.com>
2020-06-17 09:58:47 +0545

    Fix xpaths as per new UI changes

--- frontend/tests/acceptance/pages/loginPage.js
+++ frontend/tests/acceptance/pages/loginPage.js
@@ -1,15 +1,15 @@
 const { I } = inject();
 
 module.exports = {
-  url: '/login',
+  url: "/login",
   fields: {
-    email: '//label[contains(text(),"email")]/parent::div/div/input[contains(@class, "MuiInputBase-input")]',
-    password: '//label[contains(text(),"password")]/parent::div/div/input[contains(@class, "MuiInputBase-input")]',
+    email: '//label[contains(text(),"Email")]/parent::div/div/input[contains(@class, "MuiInputBase-input")]',
+    password: '//label[contains(text(),"Password")]/parent::div/div/input[contains(@class, "MuiInputBase-input")]',
   },
   login(email, password) {
     I.waitForVisible(this.fields.email);
     I.fillField(this.fields.email, email);
     I.fillField(this.fields.password, password);
-    I.click('Login');
+    I.click("Login");
   },
 };
--- frontend/tests/acceptance/pages/signupPage.js
+++ frontend/tests/acceptance/pages/signupPage.js
@@ -1,12 +1,13 @@
 const { I } = inject();
 
 module.exports = {
-  url: '/signup',
+  url: "/signup",
   fields: {
-    name: '//label[contains(text(),"name")]/parent::div/div/input[contains(@class, "MuiInputBase-input")]',
-    email: '//label[contains(text(),"email")]/parent::div/div/input[contains(@class, "MuiInputBase-input")]',
-    password: '//label[contains(text(),"password")]/parent::div/div/input[contains(@class, "MuiInputBase-input")]',
-    confirmPassword: '//label[contains(text(),"confirm password")]/parent::div/div/input[contains(@class, "MuiInputBase-input")]',
+    name: '//label[contains(text(),"Name")]/parent::div/div/input[contains(@class, "MuiInputBase-input")]',
+    email: '//label[contains(text(),"Email")]/parent::div/div/input[contains(@class, "MuiInputBase-input")]',
+    password: '//label[contains(text(),"Password")]/parent::div/div/input[contains(@class, "MuiInputBase-input")]',
+    confirmPassword:
+      '//label[contains(text(),"Confirm password")]/parent::div/div/input[contains(@class, "MuiInputBase-input")]',
   },
   signUp(name, email, password, confirmPassword) {
     I.waitForVisible(this.fields.name);
@@ -14,6 +15,6 @@ module.exports = {
     I.fillField(this.fields.email, email);
     I.fillField(this.fields.password, password);
     I.fillField(this.fields.confirmPassword, confirmPassword);
-    I.click('Sign Up');
+    I.click("Sign Up");
   },
 };
