74ba7fb506b3059f5772eecc1c1402c94b22687f
saw-jan <saw.jan.grg3e@gmail.com>
2020-06-18 11:30:12 +0545

    Add travis config to run e2e tests

--- frontend/tests/acceptance/step_definitions/signupContext.js
+++ frontend/tests/acceptance/step_definitions/signupContext.js
@@ -1,48 +1,47 @@
 const { I } = inject();
-const signupPage = require("../pages/signupPage");
-const loginPage = require("../pages/loginPage");
-const dashboard = require("../pages/dashboardPage");
-const { users } = require("../helpers/globals");
-const User = require("../helpers/api/user");
+const signupPage = require('../pages/signupPage');
+const loginPage = require('../pages/loginPage');
+const dashboard = require('../pages/dashboardPage');
+const { users } = require('../helpers/globals');
+const User = require('../helpers/api/user');
 
 const ELEMENT = signupPage.elements;
 const FIELD = signupPage.fields;
 
-Given("the user has browsed to signup page", () => {
+Given('the user has browsed to signup page', () => {
   I.amOnPage(signupPage.url);
 });
 
-Given("the user with following details already exists:", async (table) => {
+Given('the user with following details already exists:', async (table) => {
   const data = table.parse().hashes()[0];
   await User.register(data.name, data.email, data.password);
 });
 
-When("the user signs up with the following data using the webUI:", async (table) => {
+When('the user signs up with the following data using the webUI:', async (table) => {
   const data = table.parse().hashes()[0];
-  if (data.hasOwnProperty("confirmPassword"))
+  if (data.hasOwnProperty('confirmPassword'))
     await signupPage.signUp(data.name, data.email, data.password, data.confirmPassword);
   else await signupPage.signUp(data.name, data.email, data.password, data.password);
 });
 
-When("the user clicks go to login page button", () => {
+When('the user clicks go to login page button', () => {
   signupPage.goToLogin();
 });
 
-Then("the user should be redirected to login page", () => {
+Then('the user should be redirected to login page', () => {
   I.seeElement(loginPage.buttons.login);
 });
 
-Then("the user should be redirected to dashboard", async () => {
+Then('the user should be redirected to dashboard', async () => {
   I.waitForElement(dashboard.dashboardContainer, 10);
-  I.see("Dashboard");
+  I.see('Dashboard');
 });
 
-Then("the user should be able to login with email {string} and password {string}", async (email, password) => {
+Then('the user should be able to login with email {string} and password {string}', async (email, password) => {
   users.push({ email: email, password: password });
-  await User.login(email, password);
 });
 
-Then("the input fields should have following values:", (table) => {
+Then('the input fields should have following values:', (table) => {
   const data = table.parse().hashes()[0];
   I.seeInField(FIELD.name, data.name);
   I.seeInField(FIELD.email, data.email);
@@ -50,12 +49,12 @@ Then("the input fields should have following values:", (table) => {
   I.seeInField(FIELD.confirmPassword, data.confirmPassword);
 });
 
-Then("the user should not be created", () => {
+Then('the user should not be created', () => {
   I.seeInCurrentUrl(signupPage.url);
   I.dontSee(ELEMENT.error_label);
 });
 
-Then("an error message {string} should be displayed", (message) => {
+Then('an error message {string} should be displayed', (message) => {
   I.waitForElement(ELEMENT.error_label);
   I.see(message, ELEMENT.error_label);
 });
